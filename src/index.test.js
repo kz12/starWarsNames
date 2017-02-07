6492143cf5f950f44f96a773902d0b3934ccc8ab
kz12 <kz12@ic.ac.uk>
2017-02-07 15:02:04 +0800

    test(test): Change a test condition

--- src/index.test.js
+++ src/index.test.js
@@ -25,8 +25,8 @@ describe('startwars-names', function() {
     });
     
     it('should return an array of random items if passed a number', function() {
-      var randomItems = starWars.random(3);
-      expect(randomItems).to.have.length(3);
+      var randomItems = starWars.random(5);
+      expect(randomItems).to.have.length(5);
       randomItems.forEach(function(item) {
         expect(starWars.all).to.include(item);
       });
